c6d32af4f111a8e2c8dbaf12d7834d3dd023bf8f
Varun Sharma <varun05772@gmail.com>
2025-07-11 02:27:41 +0530

    Add cookie options for secure token handling in login response

--- blogshare-server/controllers/authController.js
+++ blogshare-server/controllers/authController.js
@@ -26,6 +26,8 @@ export const login = async (req, res) => {
       secure: true,
       sameSite: "None",
       maxAge: 24 * 60 * 60 * 1000,
+      path: "/",                           
+      domain: ".onrender.com",   
     });
     
 
